bce1ede58f90cbc55535d239379e2d9fccf7648a
Snoy Kuo <snoy.kuo@twprotech.com>
2021-09-16 16:19:53 +0800

    hello compose done.

--- app/src/main/java/com/snoy/hellocompose/MainActivity.kt
+++ app/src/main/java/com/snoy/hellocompose/MainActivity.kt
@@ -3,10 +3,15 @@ package com.snoy.hellocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.snoy.hellocompose.ui.theme.HelloComposeTheme
 
@@ -17,7 +22,7 @@ class MainActivity : ComponentActivity() {
             HelloComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting("Compose")
                 }
             }
         }
@@ -26,13 +31,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column(
+        Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(text = "Hello $name!", style = MaterialTheme.typography.h4)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     HelloComposeTheme {
-        Greeting("Android")
+        Surface(color = MaterialTheme.colors.background) {
+            Greeting("Compose")
+        }
     }
 }
